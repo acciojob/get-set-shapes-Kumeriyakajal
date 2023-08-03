a0206d4f47f2293f586314d97d92788d5f4ac7aa
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-08-03 21:29:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate the area of the rectangle
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Animal {
+	constructor(side) {
+    // Since Square is a subclass of Rectangle, we can call the constructor of the parent class using "super".
+    super(side, side);
+  }
+
+  // Method to calculate the perimeter of the square
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
